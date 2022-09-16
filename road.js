cd8f9f12102988211235cb78f549aa1c713d8eb9
Leo Creer <leo.creer@gmail.com>
2022-09-15 20:03:44 -0700

    init

---
+++ road.js
@@ -0,0 +1,28 @@
+class Road{
+    constructor(x,width,LaneCount=3){
+        this.x=x;
+        this.width=width;
+        this.LaneCount=LaneCount;
+
+        this.left=x-width/2;
+        this.right=x+width/2;
+
+        const infinity=1000000
+        this.top=-infinity
+        this.bottom=infinity
+    }
+    draw(ctx){
+        ctx.LineWidth=5;
+        ctx.strokeStyle="white";
+
+        ctx.beginPath();
+        ctx.moveTo(this.left,this.top);
+        ctx.lineTo(this.left,this.bottom)
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.right,this.top);
+        ctx.lineTo(this.right,this.bottom)
+        ctx.stroke();
+    }
+}
